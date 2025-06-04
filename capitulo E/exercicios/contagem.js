9a49e6d26d298a45ff391e0851a4ee4be5e8b41f
bernardohoracio <bernardohoraciorosa@gmail.com>
2025-06-04 20:25:57 -0300

    ex

--- capitulo E/exercicios/contagem.js
+++ capitulo E/exercicios/contagem.js
@@ -1,14 +1,31 @@
-function con
+function contar(){
+    var In = document.getElementById('txti')
+    var Fim = document.getElementById('txtf')
+    var Pass = document.getElementById('txtp')
+    var res = document.getElementById('res')
 
-var inicio = document.getElementById('Inicio')
-var Fim = document.getElementById('Fim')
-var P = document.getElementById('Passo')
-var res = document.getElementById('res')
-
-var i = Number(inicio.value)
-var f = Number(fim.value)
-var p = Number(passo.value)
-
-for (inicio;Fim;P++){
-    
+    if (In.value.length == 0 || Fim.value.length == 0 || Pass.value.length == 0){
+        window.alert('[ERRO] Faltam dados')
+    } else {
+        res.innerHTML = 'Contando... <br>'
+        let i = Number(In.value)
+        let f = Number(Fim.value)
+        let p = Number(Pass.value)
+        if (p <= 0){
+            window.alert('PASSO INVALIDO ')
+            p = 1
+        }
+        if (i < f){
+            for(let c = i; c <= f; c += p){
+            res.innerHTML += `  ${c}&#x1F449`
+        }
+        res.innerHTML += `&#x1F3C1`
+        } else {
+           for (let c = i; c >= f; c -= p){
+            res.innerHTML += ` ${c}&#x1F449 `
+           } 
+           res.innerHTML += `&#x1F3C1`
+        }
+        
+    }
 }
