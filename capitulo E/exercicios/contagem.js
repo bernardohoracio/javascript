3ee3a7a4a7870e4c37750ec7667f0437e97ae33f
bernardohoracio <bernardohoraciorosa@gmail.com>
2025-06-04 16:14:28 -0300

    ex

---
+++ capitulo E/exercicios/contagem.js
@@ -0,0 +1,14 @@
+function con
+
+var inicio = document.getElementById('Inicio')
+var Fim = document.getElementById('Fim')
+var P = document.getElementById('Passo')
+var res = document.getElementById('res')
+
+var i = Number(inicio.value)
+var f = Number(fim.value)
+var p = Number(passo.value)
+
+for (inicio;Fim;P++){
+    
+}
