d350733551457ef511b4b8bb45c4c23265f5e118
bernardohoracio <bernardohoraciorosa@gmail.com>
2025-06-05 17:13:02 -0300

    modulo f

---
+++ capitulo F/aula15/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5, 8 ,2 ,9, 3]
+
+console.log(`O vetor tem ${num.length} posições`)
+
+console.log(num[0])
+
+
+num.push(3)
+
+let pos = num.indexOf(5)
+console.log(`O valor 5 esta na posição ${pos}`)
+console.log(num.sort())
---
+++ capitulo F/aula15/vetornatela.js
@@ -0,0 +1,6 @@
+let v = [8, 1, 7, 4, 2, 9]
+v.sort()
+for(let pos in v){
+    console.log( v[pos])
+}
+
