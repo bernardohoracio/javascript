c04e28a1222375b7e6045c761d63749e85daecaa
bernardohoracio <bernardohoraciorosa@gmail.com>
2025-06-19 01:39:21 -0300

    js

---
+++ curso JS CFB/aula003/aula03.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let nome = 'bernardo'
+nome ='lucas'
+nome = 10
+
+const curso = 'JavaScript'
+
+console.log(nome)
---
+++ curso JS CFB/aula004/aula04.js
@@ -0,0 +1,17 @@
+let num1 = 0,num2 = 0,res = 0
+
+num1 = 2
+num2 = 15
+
+res = num1 + num2
+
+
+
+
+console.log(` ${num1} + ${num2} = ${res}`)
+console.log(num2 - num1)
+console.log(num2/num1)
+console.log(num2%num1)
+
+num1+=1
+console.log(num1)
