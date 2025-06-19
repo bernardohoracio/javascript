17c8fba1d550ad60e270612425266df6a76ddd17
bernardohoracio <bernardohoraciorosa@gmail.com>
2025-06-19 02:43:42 -0300

    aula 5

---
+++ curso JS CFB/aula005/aula05.js
@@ -0,0 +1,22 @@
+/*
+
+> maior
+
+< menor
+
+>= maior ou igual
+
+< menor ou igual
+
+== igual não é igual a =
+
+!= diferente
+
+! negação
+*/
+
+let num1 = 10
+let num2 = 5
+let num3 = 10
+
+console.log(num1 != num3)
