8be35d831e5464aa038f6712b350644561407767
bernardohoracio <bernardohoraciorosa@gmail.com>
2025-06-29 02:39:04 -0300

    aual 20

---
+++ curso JS CFB/aula019/aula019.js
@@ -0,0 +1,6 @@
+let n = 2
+do {
+    console.log('CFB cursos')
+    n++
+}while(n < 10)
+console.log('Fim do programa')
---
+++ curso JS CFB/aula020/node_c00a188151f40.tmp.js
@@ -0,0 +1,10 @@
+let n = 0
+let max = 1000
+
+while (n <= max) {
+    console.log (`CFB cursos - ${n}`)
+    if (n > 10) {
+        continue
+    } 
+    n++
+}
