57a2092b6da36514d0c552dee6b6ff072e9a288f
bernardohoracio <bernardohoraciorosa@gmail.com>
2025-07-11 00:27:48 -0300

    botoes de adicionar antes e depois

--- curso JS CFB/aula043/aula043.js
+++ curso JS CFB/aula043/aula043.js
@@ -4,12 +4,17 @@ const c1_2=document.querySelector('#c1_2')
 const cursos=['HTML','CSS','JavaScript','PHP','React','MySQL','ReactNative']
 const btnCursoSelecionado=document.getElementById('btnCursoSelecionado')
 const btnRemoverCurso=document.getElementById('btnRemoverCurso')
+const btnAdicionarNovoCursoAntes=document.getElementById('btnAdicionarNovoCursoAntes')
+const btnAdicionarNovoCursoDepois=document.getElementById('btnAdicionarNovoCursoDepois')
+const nomeCurso=document.getElementById('nomeCurso')
 
-cursos.map((el,chave)=>{
+let indice = 0
+
+const criarNovoCurso= (curso)=>{
     const novoElemento = document.createElement('div')
-    novoElemento.setAttribute('id','c'+chave)
+    novoElemento.setAttribute('id','c'+indice)
     novoElemento.setAttribute('class','curso c1')
-    novoElemento.innerHTML = el
+    novoElemento.innerHTML = curso
 
     const comandos = document.createElement('div')
     comandos.setAttribute('class','comandos')
@@ -22,7 +27,14 @@ cursos.map((el,chave)=>{
 
     novoElemento.appendChild(comandos)
 
+    return novoElemento
+}
+
+cursos.map((el,chave)=>{
+    
+    const novoElemento=criarNovoCurso(el)
     caixaCursos.appendChild(novoElemento)
+    indice++
 })
 
 const radioSelecionado = ()=>{
@@ -52,4 +64,37 @@ btnRemoverCurso.addEventListener('click',(evt)=>{
     } else {
         alert('CURSO INDEFINIDO. Selecione algum curso.')
     }
+})
+
+btnAdicionarNovoCursoAntes.addEventListener('click',()=>{
+    const rs = radioSelecionado()
+    if(rs!=undefined){
+        if (nomeCurso.value!=''){
+            const cursoSelecionado = rs.parentNode.parentNode
+            const novoCurso=criarNovoCurso(nomeCurso.value)
+            caixaCursos.insertBefore(novoCurso,cursoSelecionado)
+        } else {
+             alert('Escreva aulgum curso acima, para q possa adicionar')
+        }
+        
+    }else{
+        alert('TEXTO INDEFINIDO. Selecione algum curso.')
+    }
+
+})
+btnAdicionarNovoCursoDepois.addEventListener('click',()=>{
+     const rs = radioSelecionado()
+     
+    if (rs!=undefined) {
+        if(nomeCurso.value!=''){
+            const cursoSelecionado = rs.parentNode.parentNode
+        const novoCurso=criarNovoCurso(nomeCurso.value)
+        caixaCursos.insertBefore(novoCurso,cursoSelecionado.nextSibling)
+        }else {
+             alert('Escreva aulgum curso acima, para que possa adicionar')
+        }
+        
+    }else{
+        alert('TEXTO INDEFINIDO. Selecione algum curso.')
+        }
 })
